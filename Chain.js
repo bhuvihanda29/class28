7453335d462e0cdbc303107b2bd5cb9f73ffb3a2
bhuvihanda29 <namyahanda0712@gmail.com>
2021-07-03 16:51:07 +0530

    fly

--- Chain.js
+++ Chain.js
@@ -1,20 +1,29 @@
 class Chain{
-    constructor(bodyA, bodyB){
+    constructor(A, B){
         var options = {
-            bodyA: bodyA,
-            bodyB: bodyB,
+            bodyA: A,
+            pointB: B,
             stiffness: 0.04,
             length: 10
         }
+
         this.chain = Constraint.create(options);
         World.add(world, this.chain);
-    }
 
+        this.p = B
+    }
+    fly(){
+        this.chain.bodyA=null;
+    }
+    
     display(){
-        var pointA = this.chain.bodyA.position;
-        var pointB = this.chain.bodyB.position;
-        strokeWeight(4);
-        line(pointA.x, pointA.y, pointB.x, pointB.y);
+        if(this.chain.bodyA){
+            var pointA = this.chain.bodyA.position;
+            var pointB = this.p;
+            strokeWeight(4);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+        
     }
     
 }
